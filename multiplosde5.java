b246357b570716f21e8a43365ca5474249ea3b5c
Agnes Oliveira <110440570+oliveiraagnes@users.noreply.github.com>
2023-01-09 18:51:18 -0300

    Add files via upload

---
+++ multiplosde5.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author Aluno
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic her
+        int num = 0;
+        int cont = 1000;
+        while(num<=2000) {
+            if(num%5 == 0){
+            System.out.println(num);
+    }
+        num++;
+    } 
+    }
+}
+
+
